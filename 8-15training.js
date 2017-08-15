519fae243b2d9528eade366526fd012fc606c15a
sugarliu <450755513@qq.com>
2017-08-15 15:06:30 +0800

    use reduce method to optimize

--- 8-15training.js
+++ 8-15training.js
@@ -1,12 +1,6 @@
 function countItem(arr,item) {
-    var i = 0;
-    var count = 0;
-    while ( i < arr.length ){
-        if (arr[i] == item){
-            count++;
-        }
-        i++;
-    }
+
+    var count = arr.reduce(( count, num ) => num == item ? count + 1 : count, 0);
     console.log(count);
 }
 var arr = [1, 3, 7, 1 ,4,5,8,9,1,1,1,1];
