2eeabe400084846aa62d4fd0b6d36b5eb3855f88
sugarliu <450755513@qq.com>
2017-08-18 14:23:14 +0800

    add problem description

--- 8-17training.js
+++ 8-17training.js
@@ -1,3 +1,7 @@
+// 为数组 arr 中的每个元素求二次方。不要直接修改数组 arr，结果返回新的数组
+// 例如：arr = [5, 3, 7, 1]; 输出：[25, 9, 49, 1]
+// 要求：不要使用for，while循环
+
 function DoubleItem(arr) {
     return arr.map((function (x) {
         return x * x ;
@@ -7,3 +11,6 @@ function DoubleItem(arr) {
 arr = [5, 3, 7, 1];
 console.log(arr)
 console.log(DoubleItem(arr));
+arr1 = [2, 2, 4, 5, 6];
+console.log(arr1);
+console.log(DoubleItem(arr1));
