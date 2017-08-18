a12db579d0ddb6cc47b725cc70f9e53933cd6de4
sugarliu <450755513@qq.com>
2017-08-18 14:24:09 +0800

    add problem description

--- 8-16training.js
+++ 8-16training.js
@@ -1,7 +1,16 @@
-function findRepeatElement(arr) {
-    var result = [];
+// 题目：找出数组 arr 中重复出现过的元素
+// 例如：arr = [‘a’, ‘ab’, ‘ed’, ‘da’, ‘a’, ‘ed’, ‘b’];输出：‘a’, ‘ed’
+
+
+function findRepeatElement(array) {
+    let result = [];
+    // arr.forEach(function (item, index) {
+    //    if(arr.slice(0,index).includes(item) || arr.slice(index).includes(item)) {
+    //        result.push(item);
+    //    }
+    // });
     for (var i = 0; i < arr.length; i++) {
-        var temp = arr[i];
+        let temp = arr[i];
         for (var j = 0; j < i; j++) {
             if (temp === arr[j]) {
                 result.push(temp);
