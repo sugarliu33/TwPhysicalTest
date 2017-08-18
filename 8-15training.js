acbbc82608d6d5b3e7c5241b95ea273d4f7e303f
sugarliu <450755513@qq.com>
2017-08-18 14:25:12 +0800

    add problem description

--- 8-15training.js
+++ 8-15training.js
@@ -1,3 +1,9 @@
+// 题目：统计数组 arr 中值等于 item 的元素出现的次数
+// 例如：arr = [1, 3, 7, 1 ,4,] , item = 1,item出现的次数为2
+// 要求：不要使用for循环
+
+
+
 function countItem(arr,item) {
 
     var count = arr.reduce(( count, num ) => num == item ? count + 1 : count, 0);
