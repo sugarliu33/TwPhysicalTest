09b375a020de94d13af8e7cfa48cf148db65bacd
sugarliu <450755513@qq.com>
2017-08-29 14:14:15 +0800

    fix the code to meet the problem

--- 8-26traning.js
+++ 8-26traning.js
@@ -3,21 +3,25 @@
 //     如果这个数被5整除，打印buzz.
 //     如果这个数能同时被3和5整除，打印fizz buzz.
 function fizzBuzz(n) {
-    var ans = "";
-    if (n % 3 === 0) {
-        ans = 'fizz';
-        if (n % 5 === 0){
-            ans += ' buzz';
+    for (let i = 1; i <= n; i++){
+        var ans = "";
+        if (i % 3 === 0) {
+            ans = 'fizz';
+            if (i % 5 === 0){
+                ans += ' buzz';
+            }
+        }else if (i % 5 === 0) {
+            ans = 'buzz';
         }
-    }else if (n % 5 === 0) {
-        ans = 'buzz';
+        console.log(ans);
     }
-    return ans;
+    // return ans;
 }
 
 console.log(15);
-console.log(fizzBuzz(15));
-console.log(20);
-console.log(fizzBuzz(20));
-console.log(9);
-console.log(fizzBuzz(9));
+// console.log(fizzBuzz(15));
+fizzBuzz(15);
+// console.log(20);
+// console.log(fizzBuzz(20));
+// console.log(9);
+// console.log(fizzBuzz(9));
